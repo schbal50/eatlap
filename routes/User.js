3df265f804a0a432b05938d33580aed5019f915f
schbal50 <schbal50@gmail.com>
2020-09-07 12:02:28 +0200

    User route register refactored

--- routes/User.js
+++ routes/User.js
@@ -17,12 +17,12 @@ const signToken = userID => {
 }
 
 userRouter.post('/register', (req, res) => {
-    const { username, password, role } = req.body;
-    User.findOne({ username }, (err, user) => {
+    const { username, password, email } = req.body;
+    User.findOne({$or: [{username},{email}]}, (err, user) => {
         if (err) res.status(500).json({ message: { msgBody: "Error has occured", msgError: true } });
-        if (user) res.status(400).json({ message: { msgBody: "Username is already taken", msgError: true } });
+        if (user) res.status(400).json({ message: { msgBody: "Username or email is already in use.", msgError: true } });
         else {
-            const newUser = new User({ username, password, role });
+            const newUser = new User({ username, password, email });
             newUser.save(err => {
                 if (err) res.status(500).json({ message: { msgBody: "Error has occured", msgError: true } });
                 else res.status(201).json({ message: { msgBody: "Account successfully created", msgError: false } });
