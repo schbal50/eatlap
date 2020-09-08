6d2691c5e7417949b75a35f8389385cda28ff152
schbal50 <schbal50@gmail.com>
2020-09-08 22:51:22 +0200

    REF 5

--- models/Menu.js
+++ models/Menu.js
@@ -1,10 +1,25 @@
 const mongoose = require("mongoose");
+const valid = require('validator');
 
 // REF : add more information about the menu
 const menuSchema = new mongoose.Schema({
     name : {
         type : String, 
         required: true
+    },
+    description : {
+        type: String
+    },
+    price : {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0 ) { throw new Error('Price must be a positive number!')}
+        }
+    },
+    categories : {
+        type: [String],
+        default: undefined
     }
 })
 
