53a46d486eb73f3df15591f3ae1e9aab536a7342
schbal50 <schbal50@gmail.com>
2020-09-10 14:20:53 +0200

    REF1

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(helmet());
 app.use(morgan('common'))
 
 // REF : .env & kiszervezni külön fileba & Atlas szervert kellene használni
-mongoose.connect("mongodb://localhost:27017/mernauth", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, () => {
+mongoose.connect("mongodb+srv://schbal:W4T17ICGIE1ma2xS@eatlap.ogirp.mongodb.net/eatlap?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, () => {
     console.log("Successfully connected to database");
 });
 
